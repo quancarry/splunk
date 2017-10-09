09b4fe9487540e447d926cc22db47c425278d3cb
Quan Carry <shisuipro@gmail.com>
2017-10-09 16:22:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,13 @@
 
 #Change hostname
 
-#MD5 checksum
-    code=64b0b7dded3bf12ed3f4b344d8612070
 
 #Disable SE Linux
     setenforce 0
 
 #Edit limits config
-    sed -i "\$a soft nofile 200000" /etc/security/limits.conf
-    sed -i "\$a hard nofile 200000" /etc/security/limits.conf
+    sed -i "\$a * soft   nofile   200000" /etc/security/limits.conf
+    sed -i "\$a * hard   nofile   200000" /etc/security/limits.conf
  
 #Edit enviromment
     sed -i "\$a ip route add 172.16.5.0\/24 dev ens192 src 172.16.5.50 table rt2"  /etc/rc.d/rc.local
@@ -37,8 +35,11 @@
 #Get splunk
     wget -O splunk.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.0&product=splunk&filename=splunk-7.0.0-c8a78efdd40f-Linux-x86_64.tgz&wget=true'
 
+#MD5 checksum
+    code=64b0b7dded3bf12ed3f4b344d8612070
+    code_current=`md5sum splunk.tgz | cut -b -32`
 #Check sum
-    if [[ "$code" == "" ]];then
+    if [[ "$code_current" == "$code" ]];then
         #Install splunk
             #Untaring
             tar -zxvf splunk.tgz -C /home
